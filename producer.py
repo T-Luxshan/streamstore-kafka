a030311a0bcecc13d31b37f607428a2e2af1ac93
T-Luxshan <luckybraveboys@gmail.com>
2026-05-12 17:09:22 +0530

    producer created

---
+++ producer.py
@@ -0,0 +1,32 @@
+import json
+import uuid
+
+from confluent_kafka import Producer
+
+producer_config = {
+    "bootstrap,servers": 'localhost:9092'
+}
+
+def delivery_report(err, msg):
+    if err:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Message delivered {msg.value().decode('utf-8')}")
+
+producer = Producer(producer_config)
+
+order = {
+     "user": "Luxshan",
+    "item": "Pizza",
+    "quantity": 2
+}
+
+value = json.dumps(order).encode("utf-8")
+
+producer.produce(
+    topic="orders", 
+    value=value,
+    callback= delivery_report
+)
+
+producer.flush()
