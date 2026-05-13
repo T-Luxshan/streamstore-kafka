f12b69d4b5bd51d20b5319bf2383121749c7713d
T-Luxshan <luckybraveboys@gmail.com>
2026-05-13 10:20:18 +0530

    Function: consumer grace shutdown

--- tracker.py
+++ tracker.py
@@ -13,16 +13,21 @@ consumer = Consumer(consumer_config)
 consumer.subscribe(["orders"])
 
 print("Consumer is running and subscribed to orders topic")
+try:
+    while True:
+        msg = consumer.poll(1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            print("Error:", msg.error())
+            continue
 
-while True:
-    msg = consumer.poll(1.0)
-    if msg is None:
-        continue
-    if msg.error():
-        print("Error:", msg.error())
-        continue
+        value = msg.value().decode("utf-8")
+        order = json.loads(value)
+        print(f"Received order: {order['quantity']} x {order['item']} from {order['user']}")
 
-    value = msg.value().decode("utf-8")
-    order = json.loads(value)
-    print(f"Received order: {order['quantity']} x {order['item']} from {order['user']}")
+except KeyboardInterrupt:
+    print("\nStopping consumer....")
+finally:
+    consumer.close()
 
